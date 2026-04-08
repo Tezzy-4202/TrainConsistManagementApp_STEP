299d42ce9545e628c1caae522fa087da1d8cb135
Arun Teja <97935516+Tezzy-4202@users.noreply.github.com>
2026-04-08 08:27:20 +0530

    Update TrainConsistManagementApp.java

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,44 @@
-public class TrainConsistManagementApp {
-    public static void main(String[] args){
+import java.util.*;
 
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    void display() {
+        System.out.println(name + " -> " + capacity);
+    }
+}
+
+public class TrainConsistMgmt {
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("UC7 Sort Bogies by Capacity (Comparator)");
+        System.out.println("================================\n");
+
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("General", 90));
+
+        System.out.println("Before Sorting:");
+        for (Bogie b : bogies) {
+            b.display();
+        }
+
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        System.out.println("\nAfter Sorting by Capacity:");
+        for (Bogie b : bogies) {
+            b.display();
+        }
+
+        System.out.println("\nUC7 sorting completed...");
     }
 }
